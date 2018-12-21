16a4ad6f897375b7550081878ed4973407a1e283
Tatsuika <tatu3939104@gmail.com>
2018-12-21 20:10:58 +0900

    Initial Commit

---
+++ test_storyboard2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test_storyboard2
+//
+//  Created by 猪飼　立晟 on 2018/12/21.
+//  Copyright © 2018年 Tatsuika. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
